360a0bba63646182f250af994c01c5781334973d
Yashwanthreddy21 <yashwanthvennapusa5@gmail.com>
2025-07-13 10:57:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("formMsg").textContent = "📨 Message sent! I’ll get back to you soon.";
+  this.reset();
+});
