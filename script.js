47c880abb74d9de68e0f0b91fd793e9a777d165f
Yashwanthreddy21 <yashwanthvennapusa5@gmail.com>
2025-07-24 23:50:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,27 @@ function typeFullLine() {
 }
 
 typeFullLine();
+// ScrollSpy
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 100) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
+
 
 const links = document.querySelectorAll(".nav-link");
 const indicator = document.querySelector(".nav-indicator");
