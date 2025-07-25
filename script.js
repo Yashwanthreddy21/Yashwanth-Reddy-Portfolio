ea38473a29372233b1873bed6f07f495e6e5b22f
Yashwanthreddy21 <yashwanthvennapusa5@gmail.com>
2025-07-24 23:44:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,4 +35,26 @@ function typeFullLine() {
 
 typeFullLine();
 
+// ScrollSpy
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 100) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
+
+
 
